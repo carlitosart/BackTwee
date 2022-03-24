be1ac32a32272ede7fd0fb1dd430f999796a5ac4
carlitosart <peralta.lima.carlos@gmail.com>
2022-03-24 03:39:46 -0400

    Sin el Follow

--- src/main/java/com/example/mytwi/Follows.java
+++ src/main/java/com/example/mytwi/Follows.java
@@ -2,11 +2,4 @@ package com.example.mytwi;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "FOLLOWS")
-public class Follows {
 
-    @ManyToOne
-    @JoinColumn(name = "users_master",nullable = false)
-    private Follows users_master;
-}
